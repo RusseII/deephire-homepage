5039c1696a3efe626894395a45e2df2390ad5367
russell <russell@deephire.com>
2020-07-31 17:33:18 -0500

    allow all headers

--- api/tg.ts
+++ api/tg.ts
@@ -26,8 +26,7 @@ const allowCors = fn => async (req, res) => {
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST')
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'Access-Control-Allow-Headers', '*'
   )
   if (req.method === 'OPTIONS') {
     res.status(200).end()
