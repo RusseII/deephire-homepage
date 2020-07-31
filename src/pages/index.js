5cba89bc699d48d16c867e1d67093c26722c17c5
russell <russell@deephire.com>
2020-07-31 09:16:16 -0500

    add banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { Alert, Tag, Button } from 'antd'
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,9 +11,10 @@ import ClientOnly from '../components/clientOnly'
 
 
 const IndexPage = () => (
-   <> <SEO title="Video Interviews"/>
-    <ClientOnly>    <Page/>    </ClientOnly>
-</>
+    <> <SEO title="Video Interviews" />
+        <Alert closable style={{ textAlign: 'center' }} showIcon={false} type='info' message={<div><Tag color="blue">New</Tag> <>Live Interviews built for recruiters.</><Link style={{marginLeft: 8}} to='/live-interviews'>Learn More</Link></div>} banner />
+        <ClientOnly>    <Page />    </ClientOnly>
+    </>
 )
 
 export default IndexPage
